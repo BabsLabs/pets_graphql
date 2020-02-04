4b9fd7647ee22aeb9a6c94d19af09bb690d51995
Brian Bower <brian@brianbower.com>
2020-02-04 12:08:20 -0700

    Add querys

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 // Allows express to understand graphql and interact with graphql API
 // It is used as middleware for a single route
-const graphqlHTTP = require('express-graphql')
-
+const graphqlHTTP = require('express-graphql');
+const schema = require('./lib/schema/schema');
 
 app.use('/graphql-pets', graphqlHTTP({
-
-}))
+  schema: schema,
+  graphiql: true
+}));
 
 
 app.listen(3001, ()=> {
-  console.log("Listening for requests on port 3001")
+  console.log("Listening for requests on port 3001");
 });
